2f745e7f9cebbb19f0d81338ca56294696ff8086
Kimberley Morgan <kimberley.morgan135@gmail.com>
2021-11-15 07:13:20 +0000

    Added save functionality

--- directory.rb
+++ directory.rb
@@ -1,20 +1,18 @@
+@students = []
+
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
-    # create an empty array
-    students = []
     # get the first name
     name = gets.chomp
     # while the name is not empty, repeat this code
     while !name.empty? do
       # add the student hash to the array
-      students << {name: name, cohort: :november}
-      puts "Now we have #{students.count} students"
+      @students << {name: name, cohort: :november}
+      puts "Now we have #{@students.count} students"
       # get another name from the user
       name = gets.chomp
     end
-    # return the array of students
-    students
   end
 
 def print_header
@@ -22,8 +20,8 @@ def print_header
     puts "------------"
 end
 
-def print(students)
-    students.each do |student|
+def print_students_list
+    @students.each do |student|
       puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
 end
@@ -32,7 +30,46 @@ def print_footer(names)
     puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Save the list to students.csv"
+    puts "9. Exit" # 9 because we'll be adding more items  
+end
+
+def show_students
+    print_header
+    print_students_list
+    print_footer(@students)
+end
+
+def save_students
+    file = File.open("students.csv", "w")
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
+    file.close
+  end
+
+def interactive_menu
+    loop do
+      print_menu
+      selection = gets.chomp
+      case selection
+        when "1"
+          input_students
+        when "2"
+          show_students
+        when "3"
+          save_students
+        when "9"
+          exit
+        else
+          puts "I don't know what you meant, try again"
+      end
+    end
+  end
+
+interactive_menu
